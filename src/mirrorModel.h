67923b1219cd453673f2dc617806f184927ccdbb
jmcastelo <jose.maria.castelo@gmail.com>
2019-10-18 21:55:44 +0200

    Add copyright and license notices

--- src/mirrorModel.h
+++ src/mirrorModel.h
@@ -1,3 +1,20 @@
+// Copyright 2019 José María Castelo Ares
+
+// This file is part of qGetMirrorList.
+
+// qGetMirrorList is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// qGetMirrorList is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with qGetMirrorList.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef MIRRORMODEL_H
 #define MIRRORMODEL_H
 
